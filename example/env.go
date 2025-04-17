e154f2b9c0fb0cb96871c22877df3de92871325c
thoriqadillah <thoriqadillah59@gmail.com>
2025-04-18 00:39:03 +0700

    refactor

--- example/env.go
+++ example/env.go
@@ -5,5 +5,5 @@ import "github.com/thoriqadillah/gema"
 var (
 	APP_ENV = gema.Env("APP_ENV").String()
 	PORT    = gema.Env("APP_PORT").String(":8000")
-	DB_URL  = gema.Env("DB_URL").String("postgres://packform:packform@localhost:5432/packform?sslmode=disable")
+	DB_URL  = gema.Env("DB_URL").String("postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
 )
