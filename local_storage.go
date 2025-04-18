54a42b2c49fae17dfdbd6fe878c478c5a03ea68b
thoriqadillah <thoriqadillah59@gmail.com>
2025-04-18 13:25:07 +0700

    naming change

--- local_storage.go
+++ local_storage.go
@@ -9,23 +9,23 @@ import (
 
 const LocalStorage StorageName = "local"
 
-type local struct {
+type localStorage struct {
 	tmpDir        string
 	fullRoutePath string
 }
 
-func createLocal(option *StorageOption) Storage {
-	return &local{
+func createLocalStorage(option *StorageOption) Storage {
+	return &localStorage{
 		tmpDir:        option.TempDir,
 		fullRoutePath: option.FullRoutePath,
 	}
 }
 
-func (l *local) Serve(filename string) (io.ReadCloser, error) {
+func (l *localStorage) Serve(filename string) (io.ReadCloser, error) {
 	return os.Open(l.tmpDir + "/" + filename)
 }
 
-func (l *local) Upload(filename string, src io.Reader) (string, error) {
+func (l *localStorage) Upload(filename string, src io.Reader) (string, error) {
 	file := filepath.Join(l.tmpDir, filename)
 
 	if err := os.MkdirAll(l.tmpDir, 0755); err != nil {
@@ -46,10 +46,10 @@ func (l *local) Upload(filename string, src io.Reader) (string, error) {
 	return path, nil
 }
 
-func (l *local) Delete(filename string) error {
+func (l *localStorage) Delete(filename string) error {
 	return os.Remove(l.tmpDir + "/" + filename)
 }
 
 func init() {
-	RegisterStorage(LocalStorage, createLocal)
+	RegisterStorage(LocalStorage, createLocalStorage)
 }
