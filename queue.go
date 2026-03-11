27501828668049a4769bb8efe20c3e28411dd6ad
thoriqadillah <thoriqadillah59@gmail.com>
2026-03-11 08:31:06 +0700

    refactor queue

--- queue.go
+++ queue.go
@@ -16,7 +16,7 @@ import (
 func newClient(sql *sql.DB) *river.Client[*sql.Tx] {
 	river, err := river.NewClient(riverdatabasesql.New(sql), &river.Config{})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	return river
@@ -41,42 +41,20 @@ func newServer(queueConfig map[string]river.QueueConfig, pool *pgxpool.Pool, wor
 	return client
 }
 
-func AsWorker(constructor any) any {
-	return fx.Annotate(
-		constructor,
-		fx.As(new(river.Worker[river.JobArgs])),
-		fx.ResultTags(`group:"workers"`),
-	)
-}
-
-type queueParams struct {
-	fx.In
-
-	lc    fx.Lifecycle
-	river *river.Client[pgx.Tx]
-	w     *river.Workers
-
-	Workers []river.Worker[river.JobArgs] `group:"workers"`
-}
-
 func StartQueue(queueConfig map[string]river.QueueConfig) fx.Option {
 	return fx.Module("start_queue",
 		fx.Supply(queueConfig),
 		fx.Supply(river.NewWorkers()),
 		fx.Provide(fx.Private, newServer),
-		fx.Invoke(func(p queueParams) {
-			for _, worker := range p.Workers {
-				river.AddWorker(p.w, worker)
-			}
-
+		fx.Invoke(func(lc fx.Lifecycle, river *river.Client[pgx.Tx]) {
 			ctx, cancel := context.WithCancel(context.Background())
-			p.lc.Append(fx.Hook{
+			lc.Append(fx.Hook{
 				OnStart: func(_ context.Context) error {
-					return p.river.Start(ctx)
+					return river.Start(ctx)
 				},
 				OnStop: func(stopCtx context.Context) error {
 					cancel()
-					return p.river.Stop(stopCtx)
+					return river.Stop(stopCtx)
 				},
 			})
 		}),
